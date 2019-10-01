8240d2b72b8ce80413a7a1f445830f023bc1e481
Patrick Whitehouse <patrick1996@gmail.com>
2019-10-01 20:33:45 +0100

    init portfolio

---
+++ components/Testimonials.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      testimonial component
+  </div>
+</template>
+
+<script>
+import content from '~/assets/testimonials.json'
+export default {
+    name: 'Testimonials',
+    data(){
+        return{
+            testimonials: []
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="h-screen flex flex-col items-center justify-center">
+    <h1>Patrick Whitehouse</h1>
+    <h2>Front end developer based in Manchester, UK</h2>
+  </section>
+</template>
+<script>
+import Testimonials from '~/components/Testimonials'
+export default {
+  components:{
+    Testimonials
+  },
+  head(){
+    return{
+      title: 'Patrick Whitehouse - Front end developer based in Manchester, UK',
+      bodyAttrs: {
+        class: 'bg-gray-100 text-gray-800'
+      }
+    }
+  }
+}
+</script>
