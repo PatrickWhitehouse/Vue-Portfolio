ed42ad40637e765907f3128d4ef7078acce5ff62
Patrick Whitehouse <patrick1996@gmail.com>
2019-10-06 17:05:43 +0100

    update nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="mb-12">
-      <ul class="flex justify-end">
+      <ul class="flex justify-center md:justify-end">
           <li class="mr-6"><nuxt-link to="/">Home</nuxt-link></li>
           <li class="mr-6"><nuxt-link to="about">About</nuxt-link></li>
-          <li class="mr-6"><nuxt-link to="work">Work</nuxt-link></li>
+          <li class="md:mr-6"><nuxt-link to="work">Work</nuxt-link></li>
       </ul>
   </nav>
 </template>
