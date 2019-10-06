65bb22b63e6edc757e16163c35caa142732f032f
Patrick Whitehouse <patrick1996@gmail.com>
2019-10-06 17:45:20 +0100

    add json

---
+++ components/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="project">
+      <h2>{{client}}</h2>
+      <a :href=url>{{url}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Projects',
+    props: ['client', 'url']
+}
+</script>
---
+++ pages/work.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h1 class="text-3xl">Latest projects</h1>
+    <div>
+      <p>Have a look below at my latest projects. I'm always keen to build my portfolio, so give me a shout if you have a project in mind!</p>
+    </div>
+    <Projects v-for="project in myWork" :key="project.id" :client="project.client" :url="project.link"/>
+  </section>
+</template>
+
+<script>
+import Projects from "~/components/Projects";
+import Work from "~/assets/work";
+export default {
+  head() {
+    return {
+      title:
+        "Work - Patrick Whitehouse - Front end developer based in Manchester, UK"
+    };
+  },
+  data() {
+    return {
+      myWork: Work.projects
+    }
+  },
+  components: {
+    Projects
+  }
+}
+</script>
